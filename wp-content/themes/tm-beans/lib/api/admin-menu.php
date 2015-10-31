c2eaeda71cef89c4e2fd2a5518bf3ef0cc117612
jamestrevorlees <jamestrevorlees@gmail.com>
2015-10-31 09:26:46 +0200

    Add getbeans theme framework

---
+++ wp-content/themes/tm-beans/lib/api/admin-menu.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Beans admin page.
+ *
+ * @ignore
+ */
+class _Beans_Admin {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		add_action( 'admin_menu', array( $this, 'admin_menu' ), 150 );
+		add_action( 'admin_init', array( $this, 'register' ), 20 );
+
+	}
+
+
+	/**
+	 * Add beans menu.
+	 */
+	public function admin_menu() {
+
+		add_theme_page( __( 'Settings', 'tm-beans' ), __( 'Settings', 'tm-beans' ), 'manage_options', 'beans_settings', array( $this, 'display_screen' ) );
+
+	}
+
+
+	/**
+	 * Beans options page content.
+	 */
+	public function display_screen() {
+
+		echo '<div class="wrap">';
+
+			echo '<h2>' . __( 'Beans Settings', 'tm-beans' ) . '<span style="float: right; font-size: 10px; color: #888;">' . __( 'Version ', 'tm-beans' ) . BEANS_VERSION . '</span></h2>';
+
+			echo beans_options( 'beans_settings' );
+
+		echo '</div>';
+
+	}
+
+
+	/**
+	 * Register options.
+	 */
+	public function register() {
+
+		global $wp_meta_boxes;
+
+		$fields = array(
+			array(
+				'id' => 'beans_dev_mode',
+				'checkbox_label' => __( 'Enable development mode', 'tm-beans' ),
+				'type' => 'checkbox',
+				'description' => 'This option should be enabled while your website is in development.'
+			)
+		);
+
+		beans_register_options( $fields, 'beans_settings', 'mode_options', array(
+			'title' => __( 'Mode options', 'tm-beans' ),
+			'context' => beans_get( 'beans_settings', $wp_meta_boxes ) ? 'column' : 'normal' // Check of other beans boxes.
+		) );
+
+	}
+
+}
+
+new _Beans_Admin();
---
+++ wp-content/themes/tm-beans/lib/render/widget-area.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Registers Beans's default widget areas.
+ *
+ * @package Render\Widgets
+ */
+
+beans_add_smart_action( 'widgets_init', 'beans_do_register_widget_areas', 5 );
+
+/**
+ * Register Beans's default widget areas.
+ *
+ * @since 1.0.0
+ */
+function beans_do_register_widget_areas() {
+
+	// Keep primary sidebar first for default widget asignment.
+	beans_register_widget_area( array(
+		'name' => __( 'Sidebar Primary', 'tm-beans' ),
+		'id' => 'sidebar_primary'
+	) );
+
+	beans_register_widget_area( array(
+		'name' => __( 'Sidebar Secondary', 'tm-beans' ),
+		'id' => 'sidebar_secondary'
+	) );
+
+	if ( current_theme_supports( 'offcanvas-menu' ) )
+		beans_register_widget_area( array(
+			'name' => __( 'Off-Canvas Menu', 'tm-beans' ),
+			'id' => 'offcanvas_menu',
+			'beans_type' => 'offcanvas',
+		) );
+
+}
+
+
+/**
+ * Call register sidebar.
+ *
+ * Because WordPress.org checker don't understand that we are using register_sidebar properly,
+ * we have to add this useless call which only has to be declared once.
+ *
+ * @since 1.0.0
+ *
+ * @ignore
+ */
+add_action( 'widgets_init', 'beans_register_widget_area' );
