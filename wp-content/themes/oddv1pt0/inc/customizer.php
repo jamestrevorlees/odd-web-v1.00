c5914e3e8310adb5d51651fc5c3c561ef4526745
jamestrevorlees <jamestrevorlees@gmail.com>
2015-12-07 17:59:58 +0200

    add underscore theme framework

---
+++ wp-content/themes/oddv1pt0/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ODDV1pt0 Theme Customizer.
+ *
+ * @package ODDV1pt0
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function oddv1pt0_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'oddv1pt0_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function oddv1pt0_customize_preview_js() {
+	wp_enqueue_script( 'oddv1pt0_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+}
+add_action( 'customize_preview_init', 'oddv1pt0_customize_preview_js' );
