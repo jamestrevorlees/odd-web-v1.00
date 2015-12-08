74c567c6c383da4cef2166eb64e33ee09498fd71
jamestrevorlees <jamestrevorlees@gmail.com>
2015-12-08 16:48:27 +0200

    Add Mp4 header and Navbar

---
+++ wp-content/themes/oddv1pt0/front-page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jamestrevorlees
+ * Date: 15/12/08
+ * Time: 4:25 PM
+ */
+
+ get_header();
+
+
+?>
--- wp-content/themes/oddv1pt0/header.php
+++ wp-content/themes/oddv1pt0/header.php
@@ -26,36 +26,39 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'oddv1pt0' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+
+		<nav class="navbar navbar-default navbar-header navbar-fixed-top">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img alt="Brand" src="...">
+				</a>
+			</div>
+		</nav>
+
+
 			<?php
 
 			if ( is_front_page() && is_home() ) : ?>
-
-
+	<section>
 				<video autoplay loop poster="cookies.jpg" id="bg-video">
 					<source src="/wp-content/uploads/Open Data Wed Header_Colour_1535x400.mp4" type="video/webm">
 					<source src="/wp-content/uploads/Open Data Wed Header_Colour_1535x400.mp4" type="video/mp4">
 				</video>
+	</section>
 
-
-
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
+
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+
 			<?php
 			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
+			?>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'oddv1pt0' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
