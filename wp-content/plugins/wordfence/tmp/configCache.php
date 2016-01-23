ee8e191f1467654d9af618c127c2403c4b191f0e
jamestrevorlees <jamestrevorlees@gmail.com>
2016-01-23 18:21:45 +0200

    Add key plugins to repo

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:12:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:6:"apiKey";s:160:"51b59956bb3d64f9b0725682ab22c3bec6ff5a17e1cdce02f457feeadbbadc1c0bff7998bf103f4926ce69d5da6489633e0daab0be73d9f5f4a213f7656de630a7cafb9af6d0bb92ffc9639c765c77dd";s:20:"loginSec_lockoutMins";s:3:"240";s:19:"other_hideWPVersion";s:1:"1";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:10:"tourClosed";s:0:"";s:13:"welcomeClosed";s:1:"1";}
